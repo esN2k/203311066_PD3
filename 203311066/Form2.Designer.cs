5fbf9663944d7bf0930758fe41b4db8a9e9006c6
Doğukan Balaman <203311066@ogr.selcuk.edu.tr>
2022-10-28 11:24:00 +0300

    Finish

--- 203311066/Form2.Designer.cs
+++ 203311066/Form2.Designer.cs
@@ -35,6 +35,13 @@
             this.labelMail = new System.Windows.Forms.Label();
             this.labelSoy = new System.Windows.Forms.Label();
             this.labelIsım = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // labelTel
@@ -107,11 +114,89 @@
             this.labelIsım.TabIndex = 35;
             this.labelIsım.Text = "İsminiz*";
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Verdana", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(441, 367);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(301, 28);
+            this.label1.TabIndex = 42;
+            this.label1.Text = "Buraya CV\'nizi Yükleyin*";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Verdana", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(441, 313);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(301, 28);
+            this.label2.TabIndex = 43;
+            this.label2.Text = "Buraya CV\'nizi Yükleyin*";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Verdana", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(441, 259);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(301, 28);
+            this.label3.TabIndex = 44;
+            this.label3.Text = "Buraya CV\'nizi Yükleyin*";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Verdana", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.Location = new System.Drawing.Point(441, 205);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(301, 28);
+            this.label4.TabIndex = 45;
+            this.label4.Text = "Buraya CV\'nizi Yükleyin*";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Verdana", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label5.Location = new System.Drawing.Point(441, 151);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(301, 28);
+            this.label5.TabIndex = 46;
+            this.label5.Text = "Buraya CV\'nizi Yükleyin*";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Verdana", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label6.Location = new System.Drawing.Point(441, 97);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(301, 28);
+            this.label6.TabIndex = 47;
+            this.label6.Text = "Buraya CV\'nizi Yükleyin*";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Verdana", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label7.Location = new System.Drawing.Point(441, 43);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(301, 28);
+            this.label7.TabIndex = 48;
+            this.label7.Text = "Buraya CV\'nizi Yükleyin*";
+            this.label7.Click += new System.EventHandler(this.label7_Click);
+            // 
             // Form2
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(754, 666);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.labelTel);
             this.Controls.Add(this.labelCV);
             this.Controls.Add(this.labelKimlik);
@@ -135,5 +220,12 @@
         private System.Windows.Forms.Label labelMail;
         private System.Windows.Forms.Label labelSoy;
         private System.Windows.Forms.Label labelIsım;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
     }
 }
--- 203311066/Form2.cs
+++ 203311066/Form2.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Drawing;
 using System.Windows.Forms;
 
 namespace _203311066
@@ -9,6 +10,7 @@ namespace _203311066
         {
             InitializeComponent();
         }
+
 
         public Label LabelIsim { get; set; }
         public Label LabelSoyisim { get; set; }
@@ -26,12 +28,85 @@ namespace _203311066
 
         private void Form2_Load(object sender, EventArgs e)
         {
+            {
+                // Creating and setting the label
+                Label mylab = new Label
+                {
+                    Text = "GeeksforGeeks",
+                    Location = new Point(222, 90),
+                    AutoSize = true,
+                    Font = new Font("Calibri", 18),
+                    BorderStyle = BorderStyle.Fixed3D,
+                    ForeColor = Color.Green,
+                    Padding = new Padding(6)
+                };
+
+                // Adding this control to the form
+                Controls.Add(mylab);
+
+                // Creating and setting the label
+                Label mylab1 = new Label
+                {
+                    Text = "Welcome To GeeksforGeeks",
+                    Location = new Point(155, 170),
+                    AutoSize = true,
+                    BorderStyle = BorderStyle.Fixed3D,
+                    Font = new Font("Calibri", 18),
+                    Padding = new Padding(6)
+                };
+
+                // Adding this control to the form
+                Controls.Add(mylab1);
+            }
             LabelIsim.Text = IsimS;
             LabelSoyisim.Text = SoyisimS;
             LabelMail.Text = MailS;
             LabelTel.Text = TelS.ToString();
             LabelPoz.Text = PozS;
             LabelKimlik.Text = KimlikS.ToString();
+        }
+
+        public Form2(string text)
+        {
+            InitializeComponent();
+            label1.Text = text;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void label7_Click(object sender, EventArgs e)
+        {
+            // Creating and setting the label
+            Label mylab = new Label
+            {
+                Text = "GeeksforGeeks",
+                Location = new Point(222, 90),
+                AutoSize = true,
+                Font = new Font("Calibri", 18),
+                BorderStyle = BorderStyle.Fixed3D,
+                ForeColor = Color.Green,
+                Padding = new Padding(6)
+            };
+
+            // Adding this control to the form
+            Controls.Add(mylab);
+
+            // Creating and setting the label
+            Label mylab1 = new Label
+            {
+                Text = "Welcome To GeeksforGeeks",
+                Location = new Point(155, 170),
+                AutoSize = true,
+                BorderStyle = BorderStyle.Fixed3D,
+                Font = new Font("Calibri", 18),
+                Padding = new Padding(6)
+            };
+
+            // Adding this control to the form
+            Controls.Add(mylab1);
         }
     }
 }
